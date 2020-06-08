6f3bd47c370d2d57d24ac42a862a3f0fcc414293
Rich Walrath <richwalrath@gmail.com>
2020-06-08 14:38:06 -0700

    The Cherno - ImGui Events | Game Engine Series

--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Hazel/Layer.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+
 namespace Hazel {
 
 	class HAZEL_API ImGuiLayer : public Layer
@@ -18,6 +22,15 @@ namespace Hazel {
 
 	private:
 		float m_Time = 0.0f;
+
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
 
 	};
 
