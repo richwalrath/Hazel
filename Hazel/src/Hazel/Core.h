7e01fead36bfeb145ef59c082821a9fdcd7ab6ae
Rich Walrath <richwalrath@gmail.com>
2020-06-08 15:42:54 -0700

    The Cherno - GitHub and Hazel | Game Engine Series

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -13,6 +13,10 @@
 	#error Hazel only supports Windows!
 #endif
 
+#ifndef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif
+
 #ifdef HZ_ENABLE_ASSERTS
 	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
