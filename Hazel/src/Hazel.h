c5aa11628b295ab975eb4b709a59ff2e4329688a
Rich Walrath <richwalrath@gmail.com>
2020-06-09 13:03:44 -0700

    The Cherno - Key and Mouse Codes | Game Engine Series

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -5,8 +5,12 @@
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
-
 #include "Hazel/ImGui/ImGuiLayer.h"
+
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
 
 //-- Entry Point -------------------------//
 #include "Hazel/EntryPoint.h"
