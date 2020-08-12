4938e137f7b7f625013b651a35cb02d98aecb602
Rich Walrath <richwalrath@gmail.com>
2020-08-12 14:48:49 -0700

    The Cherno - Moving to Sandbox | Game Engine Series

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -9,15 +9,9 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
-#include "Hazel/Renderer/Shader.h"
-#include "Hazel/Renderer/Buffer.h"
-#include "Hazel/Renderer/VertexArray.h"
-
-#include "Hazel/Renderer/OrthographicCamera.h"
-
 namespace Hazel {
 
-	class HAZEL_API Application
+	class Application
 	{
 	public:
 		Application();
@@ -41,13 +35,7 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
 
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SquareVA;
-
-		OrthographicCamera m_Camera;
 
 		bool OnWindowClose(WindowCloseEvent& e);
 
