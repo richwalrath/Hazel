9b042814342ab33c31001a6ffffdcd5d30bf09c3
Rich Walrath <richwalrath@gmail.com>
2020-08-24 19:45:59 -0700

    The Cherno - Preparing for 2D Rendering

---
+++ Sandbox/src/Sandbox2D.cpp
@@ -0,0 +1,76 @@
+#include "Sandbox2D.h"
+#include <imgui/imgui.h>
+#include <glm\gtc\type_ptr.hpp>
+
+// Temp
+#include "Platform/OpenGL/OpenGLShader.h"
+
+Sandbox2D::Sandbox2D()
+	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true)
+{}
+
+void Sandbox2D::OnAttach()
+{
+	
+	// Render: SQUARE //
+
+	m_SquareVA = Hazel::VertexArray::Create();
+
+	float squareVertices[3 * 4] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
+	};
+
+	Hazel::Ref<Hazel::VertexBuffer> squareVB;
+	squareVB = Hazel::VertexBuffer::Create(squareVertices, sizeof(squareVertices));
+	squareVB->SetLayout({
+		{ Hazel::ShaderDataType::Float3, "a_Position" }
+	});
+	m_SquareVA->AddVertexBuffer(squareVB);
+
+	uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+	Hazel::Ref<Hazel::IndexBuffer> squareIB;
+	squareIB = Hazel::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
+	m_SquareVA->SetIndexBuffer(squareIB);
+
+	m_FlatColorShader = Hazel::Shader::Create("assets/shaders/FlatColor.glsl");
+
+}
+
+void Sandbox2D::OnDetach()
+{
+
+}
+
+void Sandbox2D::OnEvent(Hazel::Event& event)
+{
+	m_CameraController.OnEvent(event);
+}
+
+void Sandbox2D::OnUpdate(Hazel::Timestep ts)
+{
+	// Update
+	m_CameraController.OnUpdate(ts);
+
+	// Render
+	Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+	Hazel::RenderCommand::Clear();
+
+	Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
+
+	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
+	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
+
+	Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
+	Hazel::Renderer::EndScene();
+}
+
+void Sandbox2D::OnImGuiRender()
+{
+	ImGui::Begin("Settings");
+	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+	ImGui::End();
+}
---
+++ Sandbox/src/Sandbox2D.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Hazel.h"
+
+class Sandbox2D : public Hazel::Layer
+{
+public:
+private:
+
+	Hazel::OrthographicCameraController m_CameraController;
+
+	// Temporary
+	Hazel::Ref<Hazel::VertexArray> m_SquareVA;
+	Hazel::Ref<Hazel::Shader> m_FlatColorShader;
+	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+
+public:
+	Sandbox2D();
+	virtual ~Sandbox2D() = default;
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+
+	void OnEvent(Hazel::Event& event) override;
+	void OnUpdate(Hazel::Timestep ts) override;
+	
+	virtual void OnImGuiRender() override;
+
+private:
+
+};
