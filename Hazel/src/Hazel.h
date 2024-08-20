e743aa8157c7cc681dde080b5136fee8b2dbedfc
Rich Walrath <richwalrath@gmail.com>
2024-08-20 14:48:06 -0700

    Sets up scaffolding for an easy entry point for your application.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by applications.
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+//---- EntryPoint --------------------------//
+#include "Hazel/EntryPoint.h"
+//------------------------------------------//
